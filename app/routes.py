a4f63cd22bef0ab9c756fb57a43926a720c394b4
leonardo <leonardo.consolini@gmail.com>
2019-08-10 18:12:03 -0300

    Se realizó el tutorial https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hola mundo!"
+    user = {'username': 'Leonardo'}
+    posts = [
+        {
+            'author': {'username': 'Claudia'},
+            'body': 'Hermoso día en La Plata'
+        },
+        {
+            'author': {'username': 'Susana'},
+            'body': 'Hoy juega Argentina'
+        }
+    ]
+    return render_template('index.html', title='Inicio', user=user, posts=posts)
