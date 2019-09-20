a5b30c007b8f79fe62e51ce18b24d5472b6760f2
Leonardo Consolini <leonardo.consolini@gmail.com>
2019-09-20 12:08:34 -0300

    Se comenzo con https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 
@@ -21,8 +23,14 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Inicio de sesion requerido para el usuario {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect (url_for('index'))
+        user = User.query.filter.by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Usuario o contraseña incorrectos')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Iniciar sesión', form=form)
